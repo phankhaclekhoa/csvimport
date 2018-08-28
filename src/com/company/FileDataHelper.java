4569f7983539fe352b64a508386ee43b8451c541
pklkhoa <kphan@metabiota.com>
2018-08-28 17:27:33 +0700

    commit first

---
+++ src/com/company/FileDataHelper.java
@@ -0,0 +1,63 @@
+/*************************************************************************
+ * Copyright (c) Metabiota Incorporated - All Rights Reserved
+ * ------------------------------------------------------------------------
+ * This material is proprietary to Metabiota Incorporated. The
+ * intellectual and technical concepts contained herein are proprietary
+ * to Metabiota Incorporated. Reproduction or distribution of this
+ * material, in whole or in part, is strictly forbidden unless prior
+ * written permission is obtained from Metabiota Incorporated.
+ *************************************************************************/
+package com.company;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+import java.util.zip.GZIPInputStream;
+
+import lombok.Setter;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class FileDataHelper {
+
+//    @Setter
+//    private FilesTable filesTable = new FilesTable();
+
+    private static final Logger LOG = LogManager.getLogger(FileDataHelper.class);
+
+    private static final int GZIP_BUFFER_SIZE = 100; //TODO: move this to configurable prop
+
+//    public FilesRecord retrieveFileRecord(String fileId) throws Exception {
+//        //Retrieve file record for given fileId
+//        List<FilesRecord> fileRecords = filesTable.getRecordsById(fileId);
+//        if (!fileRecords.isEmpty() && fileRecords.size() == 1)
+//            return fileRecords.get(0);
+//        else if (fileRecords.isEmpty()) {
+//            LOG.warn("File with id: {} not found in files table", fileId);
+//            throw new PlatformException(PlatformErrorMessage.ERROR_GENERIC_MESSAGE,
+//                    "File id not found in files table: " + fileId);
+//        } else {
+//            throw new PlatformException(PlatformErrorMessage.ERROR_GENERIC_MESSAGE,
+//                    "Multiple records associated to the same fileId in files table: " + fileId);
+//        }
+//    }
+
+    public InputStream retrieveFileInputStream(FilesRecord fileRecord, String s3AccessKey, String s3SecretKey,
+                                                String s3Bucket, String s3Folder, String cacheFolder, Integer retries)
+            throws IOException, InterruptedException {
+
+        return new FileInputStream("C:/Users/pklkhoa/Downloads/Life_dupRows.csv");
+    }
+
+//    public Boolean updateFilesRecord(FilesRecord file) {
+//        try {
+//            filesTable.addOrUpdate(file);
+//            return true;
+//        } catch (Exception e) {
+//            LOG.error("Files table add/update failed!", e);
+//            return false;
+//        }
+//    }
+}
---
+++ src/com/company/FilesDataParser.java
@@ -0,0 +1,109 @@
+/*************************************************************************
+ * Copyright (c) Metabiota Incorporated - All Rights Reserved
+ * ------------------------------------------------------------------------
+ * This material is proprietary to Metabiota Incorporated. The
+ * intellectual and technical concepts contained herein are proprietary
+ * to Metabiota Incorporated. Reproduction or distribution of this
+ * material, in whole or in part, is strictly forbidden unless prior
+ * written permission is obtained from Metabiota Incorporated.
+ *************************************************************************/
+package com.company;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Optional;
+import java.util.Set;
+
+import com.metabiota.infra.upload.processing.util.csv.DataParserCsvToBeanFilter;
+import com.metabiota.infra.upload.processing.util.csv.DataParserIterableCsvToBean;
+import com.opencsv.CSVReader;
+import com.opencsv.CSVReaderBuilder;
+import com.opencsv.bean.MappingStrategy;
+import com.opencsv.enums.CSVReaderNullFieldIndicator;
+import lombok.Setter;
+import org.apache.commons.io.input.BOMInputStream;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class FilesDataParser<T> {
+
+    private static final Logger LOG = LogManager.getLogger(FilesDataParser.class);
+    private static final String DEFAULT_CHARSET = "UTF-8";
+    private final MappingStrategy<T> strategy;
+
+    private InputStream inStream;
+    private DataParserIterableCsvToBean csvToBean;
+
+    public FilesDataParser(MappingStrategy<T> strategy) {
+        this.strategy = strategy;
+    }
+
+    @Setter
+    private FileDataHelper fileDataHelper;
+
+    public Optional<Iterator<T>> getDataIterator(FilesRecord fileRecord, String s3AccessKey, String s3SecretKey, String s3Bucket, String s3Folder, String cacheFolder, Integer retries) {
+        try {
+            close();
+            if (fileDataHelper == null) {
+                fileDataHelper = new FileDataHelper();
+            }
+            inStream = fileDataHelper.retrieveFileInputStream(fileRecord, s3AccessKey, s3SecretKey, s3Bucket, s3Folder, cacheFolder, retries);
+            return Optional.of(createCSVIterator(inStream));
+        } catch (Exception e ){
+
+           // return handleInputStreamError(fileRecord, e, PlatformErrorMessage.ERROR_GENERIC_MESSAGE.getMessage());
+        }
+    }
+
+
+    private static Reader createNewReader(InputStream inputStream) throws IOException {
+        BOMInputStream bomInputStream = new BOMInputStream(inputStream);
+        if(bomInputStream.hasBOM()){
+            return new InputStreamReader(bomInputStream, bomInputStream.getBOMCharsetName());
+        } else {
+            return new InputStreamReader(bomInputStream, DEFAULT_CHARSET);
+        }
+    }
+
+    protected Iterator<T> createCSVIterator(InputStream inputStream) throws IOException {
+        Reader reader = createNewReader(inputStream);
+        CSVReaderBuilder csvBuilder = new CSVReaderBuilder(reader);
+        CSVReader csvReader = csvBuilder.withFieldAsNull(CSVReaderNullFieldIndicator.BOTH).build();
+        DataParserCsvToBeanFilter filter = new DataParserCsvToBeanFilter();
+        csvToBean = new DataParserIterableCsvToBean(csvReader, strategy, filter);
+        return csvToBean.iterator();
+    }
+
+//    private Optional<Iterator<T>> handleInputStreamError(FilesRecord file, Exception e, String errorMessage) {
+//        file.setStatus(FileStatus.INVALID);
+//        LOG.error(errorMessage, e);
+//        fileDataHelper.updateFilesRecord(file);
+//        return Optional.empty();
+//    }
+
+    public synchronized Set<String> getParseErrors(int rowNumber) {
+        if (csvToBean != null) {
+            Set<String> results = csvToBean.getParseErrors(rowNumber);
+            return results;
+        } else {
+            return new HashSet<>();
+        }
+    }
+
+    public void close() {
+        if (inStream != null) {
+            try {
+                inStream.close();
+            } catch (IOException e) {
+                LOG.error("Error closing input stream", e);
+                e.printStackTrace();
+            } finally {
+                inStream = null;
+            }
+        }
+    }
+}
